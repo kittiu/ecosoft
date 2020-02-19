abd5e0c76c65064f829ed327bbcacd2d5c4c7c46
kittiu <kittiu@gmail.com>
2020-02-19 18:46:20 +0700

    [13.0][ENH] ecosoft, add salary_export

--- ecosoft/__manifest__.py
+++ ecosoft/__manifest__.py
@@ -10,16 +10,23 @@
 * Fix SO and INV form
   * Add Tax Branch
   * Remove Tax Column
+ * Salary Export to Text File
     """,
     'author': 'Ecosoft',
     'website': 'http://ecosoft.co.th/',
     'depends': ['web',
                 'sale',
-                'l10n_th_partner'],
+                'l10n_th_partner',
+                'hr',
+                'report_csv'],
     'data': [
-        'data/report_layout.xml',
+        # Forms and Reports
+        'report/data/report_layout.xml',
         'report/invoice_report_templates.xml',
         'report/sale_report_templates.xml',
         'report/report_receipt_tax_invoice.xml',
+        # Salary Export
+        'salary_export/security/ir.model.access.csv',
+        'salary_export/views/salary_export.xml',
     ],
 }
